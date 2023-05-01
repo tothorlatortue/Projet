dbe45b791b9c2f85b5c68d5fb9942ae64540a29b
CHouingom <guillaume.campistron@hotmail.fr>
2023-05-01 18:55:00 +0200

    dczfvq

--- site/Produit.php
+++ site/Produit.php
@@ -19,6 +19,84 @@ require 'header.php';
 <body>
     <h2>LISTE DES PRODUITS.</h2>
 
-    <h3>TEST COULEUR</h3>
-</body>
+    <?php
+    
+    $user = 'root';
+    $password = 'root';
+    $db = 'distributics';
+    $host = 'localhost';
+    $connexion = mysqli_connect($host, $user, $password, $db);
+    mysqli_set_charset($connexion, "utf8");
+    if(mysqli_connect_errno())
+    {
+        echo "Problème de connexion.";
+    }
+    else 
+    {
+        echo "Connexion réussie.";
+    }
+    echo "<h1> Trier par :</h1>";
+    echo "<form method=\"post\">";
+    echo "<select name=\"menutri\" id=\"menutri\">";
+    echo "<option value=\"SELECT * from produit\">Pas de tri</option>";
+    echo "<option value=\"SELECT * from produit where typeP = 1 \">Nourriture</option>";
+    echo "<option value=\"SELECT * from produit where typeP = 2 \">Boisson Chaude</option>";
+    echo "<option value=\"SELECT * from produit where typeP = 3 \">Boisson Froide</option>";
+    echo "<option value=\"SELECT * from produit Order by prixP Desc\">Trié par prix décroissant</option>";
+    echo "</select>";
+    echo "<input type=\"submit\" name=\"afficher\"  value=\"Afficher\"/>";
+    echo "</form>";
+
+
+    if (isset($_POST['menutri'])) 
+    {
+        $requete1 = $_POST['menutri'];
+    }
+    if (empty($selected)) 
+    {
+         $selected = '';
+    }
+    $reponse1 = mysqli_query($connexion,$requete1);
+    
+
+
+    $ligne = mysqli_fetch_array($reponse1);
+        if($ligne != NULL)
+        {echo "<table border>";
+            echo "<tr>";
+            echo "<td><h2>Photo</h2></td>";
+            echo "<td><h2>Marque</h2></td>";
+            echo "<td><h2>Nom</h2></td>";
+            echo "<td><h2>Type</h2></td>";
+            echo "<td><h2>Prix</h2></td>";
+            echo "</tr>";
+                while($ligne != NULL)
+                {
+                    echo "<tr>";
+                
+                        echo "<td><img src=photo/photo_produit/".$ligne['photoP']." height=\"200px\" /></td>";
+                        echo "<td><h3>".$ligne['marqueP']."</h3></td>";
+                        echo "<td><h3>".$ligne['nomP']."</h3></td>";
+                        if ($ligne['typeP']==1 )
+                        echo "<td><h3>Nourriture</h3></td>";
+                        if ($ligne['typeP']==2 )
+                        echo "<td><h3>Boisson Chaude</h3></td>";
+                        if ($ligne['typeP']==3 )
+                        echo "<td><h3>Boisson Froide</h3></td>";
+                        echo "<td><h3>".$ligne['prixP']." €</h3></td>";
+                    
+                    $ligne = mysqli_fetch_array($reponse1);
+                    echo "</tr>";
+                    
+                }
+        }
+        else
+        {
+            echo "<tr><td><h3>Aucun produit.</h3></td></tr>";
+        }
+    mysqli_free_result($reponse1);
+
+    mysqli_close($connexion);
+  ?>
+    </body>
 </html>
--- site/liste_utilisateur.php
+++ site/liste_utilisateur.php
@@ -17,12 +17,69 @@ if(!isset($_SESSION["nom"]))
 require 'header.php'; 
 ?>
 <body> 
-    <h2> Recherche d'un utilisateur </h2>
-     <form method="POST" action="listeutilisateur.php">
-     <fieldset>
-        Adresse mail de l'utilisateur : </strong><input name="adressemail"><br>
-        <br><input name="listeutilisateur" type="submit" value="Rechercher"/>
-    </fieldset>
-    </form>
+<h1> <center>Liste des utilisateurs</center></h1>
+<?php
+$user = 'root';
+    $password = 'root';
+    $db = 'distributics';
+    $host = 'localhost';
+    $connexion = mysqli_connect($host, $user, $password, $db);
+    mysqli_set_charset($connexion, "utf8");
+    if(mysqli_connect_errno())
+    {
+        echo "Problème de connexion.";
+    }
+    else 
+    {
+        echo "Connexion réussie.";
+    }
+
+    $requete="SELECT mail, pass, nom, prenom, typec, photo FROM utilisateur;";
+
+    $reponse = mysqli_query($connexion, $requete);
+	if($reponse)
+		echo "Exécution réussie" ;
+	else
+		echo "Exécution non réussie" ;
+	
+	$datas = mysqli_fetch_all($reponse);
+
+    echo "<table border>";
+    echo "<tr>";
+    echo "<td><h2>Mail</h2></td>";
+    echo "<td><h2>Mot de passe</h2></td>";
+    echo "<td><h2>Nom</h2></td>";
+    echo "<td><h2>Prenom</h2></td>";
+    echo "<td><h2>Catégorie</h2></td>";
+	echo "<td><h2>Photo</h2></td>";
+    echo "</tr>";
+
+	foreach($datas as $ligne)
+    {
+        echo "<tr>";
+        echo "<td><h3>".$ligne['0']."</h3></td>";
+        echo "<td><h3>".$ligne['1']."</h3></td>";
+        echo "<td><h3>".$ligne['2']."</h3></td>";
+        echo "<td><h3>".$ligne['3']."</h3></td>";
+        if ($ligne['4']==1 )
+        echo "<td><h3>Etudiant</h3></td>";
+        if ($ligne['4']==2 )
+        echo "<td><h3>Personnel</h3></td>";  
+        if ($ligne['4']==3 )
+        echo "<td><h3>Enseignant</h3></td>";
+        if ($ligne['4']==4 )
+        echo "<td><h3>Doctorant</h3></td>";                  
+		if ($ligne['5'])
+			echo "<td><img src=photo/photo_user/".$ligne['5']." height=\"100px\" width=\"100px\"/></td>";
+		else
+			echo "<td><img src=photo/photo_user/anonyme.png".$ligne['5']." height=\"100px\" width=\"100px\"/></td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    mysqli_free_result($reponse);
+
+    mysqli_close($connexion);
+    ?>
 </body>
 </html>
